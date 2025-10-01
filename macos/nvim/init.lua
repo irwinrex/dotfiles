6dbbd9f728ff62d3328ec46d05d89b01abff4863
IrwinRex <irwinrex.a@gmail.com>
2025-10-01 10:21:22 +0530

    removed old files

--- macos/nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
---lazyconfig require("config.options")
---require("config.autocmds")
--- require("config.theme")
--- macos/nvim/lua/config/autocmds.lua
+++
@@ -1,7 +0,0 @@
--- highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
