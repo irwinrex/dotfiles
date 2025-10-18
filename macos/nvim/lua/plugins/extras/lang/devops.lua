56fb93eba15bc710037b96da8b40e573c382f2a7
Irwin Rex <irwin.a@equipohealth.com>
2025-10-18 15:37:54 +0530

    macos : updated go lang lsp

--- macos/nvim/lua/plugins/extras/lang/devops.lua
+++ macos/nvim/lua/plugins/extras/lang/devops.lua
@@ -1,65 +1,54 @@
 return {
+  -- 1. Custom filetype detection for Jenkinsfiles
   {
-    "LazyVim/LazyVim",
+    "nvim-treesitter/nvim-treesitter", -- Hook into a core plugin to ensure this runs early
     init = function()
-      vim.filetype.add({
-        filename = {
-          ["Jenkinsfile"] = "groovy",
-        },
-        pattern = {
-          ["Jenkinsfile.*"] = "groovy",
-          [".*%.jenkinsfile"] = "groovy",
-        },
+      -- Associate Jenkinsfile patterns with the 'groovy' filetype
+      vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+        pattern = { "Jenkinsfile", "Jenkinsfile.*", ".*%.jenkinsfile" },
+        desc = "Set Jenkinsfile filetype to groovy",
+        command = "set filetype=groovy",
       })
     end,
   },
-
+  -- 2. LSP server configurations
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "b0o/schemastore.nvim",
-    },
+    dependencies = { "b0o/schemastore.nvim" },
     opts = function(_, opts)
       opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
+        -- Configure YAML language server with schemas from schemastore
         yamlls = {
           settings = {
             yaml = {
-              validate = true,
-              hover = true,
-              completion = true,
               schemas = require("schemastore").yaml.schemas(),
-              format = {
-                enable = true,
-              },
             },
           },
         },
-        dockerls = {
-          settings = {
-            docker = {
-              languageserver = {
-                formatter = {
-                  ignoreMultilineInstructions = true,
-                },
-              },
-            },
-          },
-        },
-        groovyls = {
-          filetypes = { "groovy" },
-        },
+        -- For dockerls and groovyls, we accept LazyVim's sensible defaults
+        dockerls = {},
+        groovyls = {},
       })
       return opts
     end,
   },
 
+  -- 3. Linter configurations
   {
     "mfussenegger/nvim-lint",
     opts = function(_, opts)
+      -- Override default arguments for specific linters
+      opts.linters = opts.linters or {}
+      -- Disable the 'document-start' rule for yamllint
+      opts.linters.yamllint = {
+        args = { "-d", "document-start" },
+      }
+
+      -- Assign linters to specific filetypes
       opts.linters_by_ft = vim.tbl_deep_extend("force", opts.linters_by_ft or {}, {
-        yaml = { "yamllint" },
         dockerfile = { "hadolint" },
       })
+
       return opts
     end,
   },
--- macos/nvim/lua/plugins/extras/lang/go.lua
+++ macos/nvim/lua/plugins/extras/lang/go.lua
@@ -1,124 +1,74 @@
 return {
+  -- Go LSP setup
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        gopls = {
-          settings = {
-            gopls = {
-              staticcheck = true,
-              usePlaceholders = true,
-              completeUnimported = true,
-              matcher = "Fuzzy",
-              gofumpt = true,
-              experimentalPostfixCompletions = true,
-              experimentalWorkspaceModule = true,
-              vulncheck = "Imports",
-              codelenses = {
-                generate = true,
-                test = true,
-                tidy = true,
-                upgrade_dependency = true,
-                vendor = true,
-              },
-              semanticTokens = true,
-              hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-              },
-              analyses = {
-                unusedparams = true,
-                unreachable = true,
-                shadow = true,
-                useany = true,
-                unusedvariable = true,
-                unusedwrite = true,
-                nilness = true,
-                printf = true,
-                shift = true,
-                simplifycompositelit = true,
-                simplifyrange = true,
-                simplifyslice = true,
-                sortslice = true,
-                testinggoroutine = true,
-                timeformat = true,
-                undeclaredname = true,
-                unusedresult = true,
-              },
-              buildFlags = { "-tags", "integration" },
-              env = {
-                GOFLAGS = "-tags=integration",
-                CGO_ENABLED = "0",
-              },
-              diagnosticsDelay = "250ms",
-              diagnosticsTrigger = "Edit",
-              analysisProgressReporting = true,
-              templateExtensions = { "html", "tmpl", "gotmpl" },
-              memoryMode = "DegradeClosed",
-              deepCompletion = true,
-              fuzzyMatching = true,
-              caseSensitiveCompletion = false,
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.gopls = {
+        settings = {
+          gopls = {
+            staticcheck = true,
+            usePlaceholders = true,
+            completeUnimported = true,
+            matcher = "Fuzzy",
+            gofumpt = true,
+            experimentalPostfixCompletions = true,
+            experimentalWorkspaceModule = true,
+            vulncheck = "Imports",
+            codelenses = {
+              generate = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
             },
+            semanticTokens = true,
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+            analyses = {
+              unusedparams = true,
+              unreachable = true,
+              shadow = true,
+              useany = true,
+              unusedvariable = true,
+              unusedwrite = true,
+              nilness = true,
+              printf = true,
+              shift = true,
+              simplifycompositelit = true,
+              simplifyrange = true,
+              simplifyslice = true,
+              sortslice = true,
+              testinggoroutine = true,
+              timeformat = true,
+              undeclaredname = true,
+              unusedresult = true,
+            },
+            buildFlags = { "-tags", "integration" },
+            env = {
+              GOFLAGS = "-tags=integration",
+              CGO_ENABLED = "0",
+            },
+            diagnosticsDelay = "250ms",
+            diagnosticsTrigger = "Edit",
+            analysisProgressReporting = true,
+            templateExtensions = { "html", "tmpl", "gotmpl" },
+            memoryMode = "DegradeClosed",
+            deepCompletion = true,
+            fuzzyMatching = true,
+            caseSensitiveCompletion = false,
           },
         },
-      },
-    },
-  },
+      }
+    end,
 
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        go = { "golangci_lint" },
-      },
-      linters = {
-        golangci_lint = {
-          cmd = "golangci-lint",
-          args = {
-            "run",
-            "--out-format=json",
-            "--print-issued-lines=false",
-            "--print-linter-name=true",
-            "--uniq-by-line=false",
-            "--max-issues-per-linter=0",
-            "--max-same-issues=0",
-            function()
-              return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
-            end,
-          },
-        },
-      },
-    },
-  },
-
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        go = { "goimports", "gofumpt" },
-      },
-      formatters = {
-        goimports = {
-          args = { "-local", "github.com/yourorg" },
-        },
-        gofumpt = {
-          args = { "-extra" },
-        },
-      },
-      format_on_save = {
-        timeout_ms = 1000,
-        lsp_fallback = true,
-      },
-    },
-  },
-
-  {
-    "LazyVim/LazyVim",
     keys = {
       { "<leader>cR", "<cmd>GoRun<cr>", desc = "Go Run", ft = "go" },
       { "<leader>ct", "<cmd>GoTest<cr>", desc = "Go Test", ft = "go" },
@@ -129,54 +79,58 @@ return {
       { "<leader>cm", "<cmd>GoMod<cr>", desc = "Go Mod Tidy", ft = "go" },
       { "<leader>cv", "<cmd>GoVulnCheck<cr>", desc = "Go Vulnerability Check", ft = "go" },
       { "<leader>cw", "<cmd>GoWorkspace<cr>", desc = "Go Workspace", ft = "go" },
-      { "<leader>cI", "<cmd>GoImplInterface<cr>", desc = "Implement Interface", ft = "go" },
-      { "<leader>cF", "<cmd>GoFillStruct<cr>", desc = "Fill Struct", ft = "go" },
     },
+
     init = function()
-      vim.api.nvim_create_user_command("GoRun", function()
-        vim.cmd("!go run " .. vim.fn.expand("%"))
+      -- === Custom Go Commands ===
+      local cmd = vim.api.nvim_create_user_command
+
+      cmd("GoRun", function()
+        vim.fn.system({ "go", "run", vim.fn.expand("%") })
+        vim.notify("Go Run executed", vim.log.levels.INFO)
       end, {})
 
-      vim.api.nvim_create_user_command("GoTest", function()
-        vim.cmd("!gotestsum --format=testname ./...")
+      cmd("GoTest", function()
+        vim.fn.system({ "gotestsum", "--format=testname", "./..." })
+        vim.notify("Running all tests...", vim.log.levels.INFO)
       end, {})
 
-      vim.api.nvim_create_user_command("GoTestFunc", function()
+      cmd("GoTestFunc", function()
         local func_name = vim.fn.expand("<cword>")
-        vim.cmd("!gotestsum --format=testname -- -run=" .. func_name)
+        vim.fn.system({ "gotestsum", "--format=testname", "--", "-run=" .. func_name })
+        vim.notify("Testing function: " .. func_name, vim.log.levels.INFO)
       end, {})
 
-      vim.api.nvim_create_user_command("GoTestAll", function()
-        vim.cmd("!gotestsum --format=standard-verbose ./...")
+      cmd("GoTestAll", function()
+        vim.fn.system({ "gotestsum", "--format=standard-verbose", "./..." })
       end, {})
 
-      vim.api.nvim_create_user_command("GoCoverage", function()
-        vim.cmd("!go test -coverprofile=coverage.out ./... && go tool cover -html=coverage.out")
+      cmd("GoCoverage", function()
+        vim.fn.system({ "go", "test", "-coverprofile=coverage.out", "./..." })
+        vim.fn.system({ "go", "tool", "cover", "-html=coverage.out" })
       end, {})
 
-      vim.api.nvim_create_user_command("GoGenerate", function()
-        vim.cmd("!go generate ./...")
+      cmd("GoGenerate", function()
+        vim.fn.system({ "go", "generate", "./..." })
       end, {})
 
-      vim.api.nvim_create_user_command("GoMod", function()
-        vim.cmd("!go mod tidy && go mod verify")
+      cmd("GoMod", function()
+        vim.fn.system({ "go", "mod", "tidy" })
+        vim.fn.system({ "go", "mod", "verify" })
+        vim.notify("Go modules synced", vim.log.levels.INFO)
       end, {})
 
-      vim.api.nvim_create_user_command("GoVulnCheck", function()
-        vim.cmd("!govulncheck ./...")
+      cmd("GoVulnCheck", function()
+        vim.fn.system({ "govulncheck", "./..." })
+        vim.notify("Vulnerability check complete", vim.log.levels.INFO)
       end, {})
 
-      vim.api.nvim_create_user_command("GoWorkspace", function()
-        vim.cmd("!go work sync")
+      cmd("GoWorkspace", function()
+        vim.fn.system({ "go", "work", "sync" })
+        vim.notify("Workspace synced", vim.log.levels.INFO)
       end, {})
 
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.go",
-        callback = function()
-          vim.lsp.buf.format({ timeout_ms = 1000 })
-        end,
-      })
-
+      -- Go file settings
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "go",
         callback = function()
@@ -184,6 +138,61 @@ return {
           vim.opt_local.tabstop = 4
           vim.opt_local.shiftwidth = 4
           vim.opt_local.softtabstop = 4
+        end,
+      })
+    end,
+  },
+
+  -- GolangCI-Lint setup
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      local lint = require("lint")
+
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.go = { "golangci_lint" }
+
+      lint.linters.golangci_lint = {
+        cmd = "golangci-lint",
+        stdin = false,
+        args = {
+          "run",
+          "--out-format",
+          "json",
+          "--issues-exit-code=1",
+          "--print-issued-lines=false",
+          "--print-linter-name=true",
+          "--max-issues-per-linter=0",
+          "--max-same-issues=0",
+          "--path-prefix",
+          vim.fn.getcwd(),
+        },
+        ignore_exitcode = true, -- prevents breaking on exit code 3
+        parser = function(output, bufnr)
+          local ok, decoded = pcall(vim.json.decode, output)
+          if not ok or not decoded or not decoded.Issues then
+            return {}
+          end
+
+          local diagnostics = {}
+          for _, issue in ipairs(decoded.Issues) do
+            table.insert(diagnostics, {
+              lnum = issue.Pos.Line - 1,
+              col = issue.Pos.Column - 1,
+              message = string.format("[%s] %s", issue.FromLinter or "golangci", issue.Text),
+              severity = vim.diagnostic.severity.WARN,
+              source = "golangci-lint",
+            })
+          end
+          return diagnostics
+        end,
+      }
+
+      -- Optional: auto-lint on save
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        pattern = "*.go",
+        callback = function()
+          require("lint").try_lint()
         end,
       })
     end,
