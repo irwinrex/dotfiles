e5c194f5b52357dab776c7d6e671022d7a1685b9
Irwin Rex <irwin.a@equipohealth.com>
2025-10-28 18:21:16 +0530

    macos : removed unnessary files

--- macos/nvim/nvim/lua/config/treesitter.lua
+++ macos/nvim/nvim/lua/config/treesitter.lua
@@ -88,7 +88,7 @@ return {
     -- Disable for large files and less common languages
     disable = function(lang, buf)
       local max_filesize = 200 * 1024 -- 200 KB (increased from 100KB)
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
         return true
       end
