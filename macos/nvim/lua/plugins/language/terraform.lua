b6972e2b3eebdf6b928923131cdd34924becf5fc
Irwin Rex <irwin.a@equipohealth.com>
2025-11-17 11:41:44 +0530

    updated lsp

--- macos/nvim/lua/plugins/language/terraform.lua
+++ macos/nvim/lua/plugins/language/terraform.lua
@@ -1,15 +1,22 @@
 -- plugins/language/terraform.lua
 
 return {
-  -- Ensure Terraform LSP is always installed
+  ---------------------------------------------------------------------------
+  -- Mason registry (ensure Terraform tools exist)
+  ---------------------------------------------------------------------------
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "terraformls", "tflint" },
+      ensure_installed = {
+        "terraformls",
+        "tflint",
+      },
     },
   },
 
-  -- Use conform.nvim for formatting
+  ---------------------------------------------------------------------------
+  -- Formatting via conform.nvim (terraform fmt)
+  ---------------------------------------------------------------------------
   {
     "stevearc/conform.nvim",
     ft = { "terraform", "tf", "hcl", "terraform-vars" },
@@ -17,40 +24,47 @@ return {
       formatters_by_ft = {
         terraform = { "terraform_fmt" },
         tf = { "terraform_fmt" },
-        hcl = { "packer_fmt" },
+        hcl = { "packer_fmt" }, -- packer .hcl
         ["terraform-vars"] = { "terraform_fmt" },
       },
-      -- REMOVE format_on_save, let LazyVim handle it!
+      -- No format_on_save here — LazyVim controls it globally
     },
   },
 
-  -- nvim-lint for linting
+  ---------------------------------------------------------------------------
+  -- Linting: terraform_validate (schema + HCL sanity)
+  ---------------------------------------------------------------------------
   {
     "mfussenegger/nvim-lint",
     event = { "BufReadPre", "BufNewFile" },
-    ft = { "terraform", "tf" },
+    ft = { "terraform", "tf", "hcl" },
     opts = {
       linters_by_ft = {
         terraform = { "terraform_validate" },
         tf = { "terraform_validate" },
+        hcl = { "terraform_validate" },
       },
     },
   },
 
-  -- Telescope extension: terraform-doc
+  ---------------------------------------------------------------------------
+  -- Telescope integration: Terraform documentation browser
+  ---------------------------------------------------------------------------
   {
     "ANGkeith/telescope-terraform-doc.nvim",
-    ft = { "terraform", "hcl" },
+    ft = { "terraform", "hcl", "tf" },
     dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
       require("telescope").load_extension("terraform_doc")
     end,
   },
 
-  -- Telescope extension: terraform state browsing
+  ---------------------------------------------------------------------------
+  -- Telescope integration: Terraform file / state browser
+  ---------------------------------------------------------------------------
   {
     "cappyzawa/telescope-terraform.nvim",
-    ft = { "terraform", "hcl" },
+    ft = { "terraform", "hcl", "tf" },
     dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
       require("telescope").load_extension("terraform")
--- macos/nvim/lua/plugins/lsp.lua
+++ macos/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,9 @@
 -- plugins/lsp.lua
 
 return {
-  -- Mason v2.1.0 - Latest Package Manager (Nov 2025)
+  ---------------------------------------------------------------------------
+  -- Mason v2.1.0 - Package Manager
+  ---------------------------------------------------------------------------
   {
     "mason-org/mason.nvim",
     lazy = false,
@@ -15,7 +17,9 @@ return {
     },
   },
 
-  -- Mason LSPConfig - Now tightly integrated with mason v2.1.0
+  ---------------------------------------------------------------------------
+  -- Mason LSPConfig - Automatic LSP installation
+  ---------------------------------------------------------------------------
   {
     "mason-org/mason-lspconfig.nvim",
     lazy = false,
@@ -37,15 +41,20 @@ return {
         "tflint",
       },
       automatic_install = true,
+
       handlers = {
+        ---------------------------------------------------------------------
+        -- Default LSP setup (Blink auto-injects capabilities)
+        ---------------------------------------------------------------------
         function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-          })
+          require("lspconfig")[server_name].setup({})
         end,
+
+        ---------------------------------------------------------------------
+        -- Lua LS
+        ---------------------------------------------------------------------
         lua_ls = function()
           require("lspconfig").lua_ls.setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
             settings = {
               Lua = {
                 diagnostics = { globals = { "vim" } },
@@ -55,9 +64,12 @@ return {
             },
           })
         end,
+
+        ---------------------------------------------------------------------
+        -- BasedPyright
+        ---------------------------------------------------------------------
         basedpyright = function()
           require("lspconfig").basedpyright.setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
             settings = {
               python = {
                 analysis = {
@@ -69,9 +81,12 @@ return {
             },
           })
         end,
+
+        ---------------------------------------------------------------------
+        -- Go (gopls)
+        ---------------------------------------------------------------------
         gopls = function()
           require("lspconfig").gopls.setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
             settings = {
               gopls = {
                 usePlaceholders = true,
@@ -81,98 +96,70 @@ return {
             },
           })
         end,
+
+        ---------------------------------------------------------------------
+        -- Terraform LSP
+        ---------------------------------------------------------------------
         terraformls = function()
           require("lspconfig").terraformls.setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
             settings = {
               terraform = {
                 hashicorp = {
                   terraform = {
-                    validation = {
-                      enableEnhancedValidation = true,
-                    },
+                    validation = { enableEnhancedValidation = true },
                   },
                 },
               },
             },
           })
         end,
+
+        ---------------------------------------------------------------------
+        -- TFLint
+        ---------------------------------------------------------------------
         tflint = function()
-          require("lspconfig").tflint.setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-          })
+          require("lspconfig").tflint.setup({})
         end,
       },
     },
   },
 
-  -- LSPConfig v2 - Core LSP client for Neovim v0.11
+  ---------------------------------------------------------------------------
+  -- Core LSPConfig - Keymaps, diagnostics, formatting integration
+  ---------------------------------------------------------------------------
   {
     "neovim/nvim-lspconfig",
     lazy = false,
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
     config = function()
+      -----------------------------------------------------------------------
+      -- Keymaps for LSP
+      -----------------------------------------------------------------------
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("LspKeymaps", { clear = true }),
         callback = function(event)
           local opts = { buffer = event.buf, silent = true }
 
-          vim.keymap.set(
-            "n",
-            "gd",
-            vim.lsp.buf.definition,
-            vim.tbl_extend("force", opts, { desc = "Go to definition" })
-          )
-          vim.keymap.set(
-            "n",
-            "gD",
-            vim.lsp.buf.declaration,
-            vim.tbl_extend("force", opts, { desc = "Go to declaration" })
-          )
-          vim.keymap.set(
-            "n",
-            "gi",
-            vim.lsp.buf.implementation,
-            vim.tbl_extend("force", opts, { desc = "Go to implementation" })
-          )
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Find references" }))
-          vim.keymap.set(
-            "n",
-            "gt",
-            vim.lsp.buf.type_definition,
-            vim.tbl_extend("force", opts, { desc = "Go to type definition" })
-          )
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover documentation" }))
-          vim.keymap.set(
-            "n",
-            "<leader>ca",
-            vim.lsp.buf.code_action,
-            vim.tbl_extend("force", opts, { desc = "Code action" })
-          )
-          vim.keymap.set(
-            "n",
-            "<leader>rn",
-            vim.lsp.buf.rename,
-            vim.tbl_extend("force", opts, { desc = "Rename symbol" })
-          )
-          vim.keymap.set("n", "<leader>f", function()
-            vim.lsp.buf.format({ async = true })
-          end, vim.tbl_extend("force", opts, { desc = "Format code" }))
-          vim.keymap.set("n", "<leader>f", function()
-            vim.lsp.buf.format({ async = true })
-          end, vim.tbl_extend("force", opts, { desc = "Format code" }))
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+          -- Format (LSP or fallback formatter)
           vim.keymap.set("n", "<leader>f", function()
             require("conform").format({ async = true, lsp_fallback = true })
-          end, vim.tbl_extend("force", opts, { desc = "Format code" }))
-          vim.keymap.set(
-            "n",
-            "<leader>d",
-            vim.diagnostic.open_float,
-            vim.tbl_extend("force", opts, { desc = "Show diagnostics" })
-          )
+          end, opts)
+
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
         end,
       })
 
+      -----------------------------------------------------------------------
+      -- Diagnostic signs
+      -----------------------------------------------------------------------
       local signs = { Error = "✘", Warn = "▲", Hint = "◆", Info = "●" }
       for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
@@ -181,65 +168,26 @@ return {
     end,
   },
 
-  -- Completion plugin (hrsh7th/nvim-cmp)
+  ---------------------------------------------------------------------------
+  -- Completion Engine: blink.cmp (Replaces nvim-cmp fully)
+  ---------------------------------------------------------------------------
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
+    "saghen/blink.cmp",
+    lazy = false,
+    version = "*",
+    opts = {
+      keymap = {
+        preset = "default",
+      },
+      completion = {
+        documentation = { auto_show = true },
+      },
+      signature = {
+        enabled = true,
+      },
+      sources = {
+        default = { "lsp", "path", "buffer" },
+      },
     },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
-
-  -- Snippet engine
-  {
-    "L3MON4D3/LuaSnip",
-    lazy = true,
   },
 }
