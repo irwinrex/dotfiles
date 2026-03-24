05c3e7d4ed4ae66de282f347d0b04fc959422125
Irwin Rex <irwin.a@equipohealth.com>
2026-03-24 15:41:23 +0530

    nvim : updated dif checker

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,17 @@
 -- vim.opt.relativenumber = false
 -- Disable true color — use terminal's 16 ANSI colors instead
 vim.opt.termguicolors = false
+
+-- Diff options
+vim.opt.diffopt:append({
+  "internal",
+  "filler",
+  "closeoff",
+  "indent-heuristic",
+  "algorithm:histogram",
+})
+
+-- linematch only available in nvim 0.9+
+if vim.fn.has("nvim-0.9") == 1 then
+  vim.opt.diffopt:append({ "linematch:60" })
+end
