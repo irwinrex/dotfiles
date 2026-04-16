f5ee3af0512142f8c5d3226d8b4fe49aac5bf59f
Irwin Rex <irwin.a@equipohealth.com>
2026-04-16 12:55:22 +0530

    NVIM : again updated some latest pratices for terraform.lua

--- nvim/.config/nvim/lua/plugins/lsp/terraform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/terraform.lua
@@ -1,11 +1,14 @@
--- Terraform/HCL LSP and tooling
+-- lua/plugins/terraform.lua
 return {
+  -- ─── LSP: terraformls ────────────────────────────────────────────────
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         terraformls = {
-          root_dir = require("lspconfig.util").root_pattern("terragrunt.hcl", ".terraform", ".git", "main.tf"),
+          root_dir = require("lspconfig.util").root_pattern("terragrunt.hcl", ".terraform", ".git", "main.tf", "*.tf"),
+
+          -- Key fix: watch all .tf file changes across workspace automatically
           capabilities = {
             workspace = {
               didChangeWatchedFiles = {
@@ -13,44 +16,39 @@ return {
               },
             },
           },
-          on_attach = function(client)
+
+          on_attach = function(client, _)
+            -- Let conform.nvim handle formatting, not LSP
             client.server_capabilities.documentFormattingProvider = false
             client.server_capabilities.documentRangeFormattingProvider = false
           end,
+
           settings = {
+            ["terraform-ls"] = {
+              -- Don't require terraform init to get basic LSP features
+              ignoreSingleFileWarning = true,
+              -- Disable noisy validation in monorepos / uninitialized workspaces
+              validation = {
+                enableEnhancedValidation = false,
+              },
+              -- Experimental but useful for large monorepos
+              indexing = {
+                ignorePaths = {
+                  ".terraform",
+                  "**/.terraform",
+                },
+                ignoreDirectoryNames = {
+                  ".terraform",
+                  "node_modules",
+                  ".git",
+                },
+              },
+            },
             terraform = {
               experimentalFeatures = {
                 prefillRequiredFields = true,
               },
             },
-            ["terraform-ls"] = {
-              validation = {
-                enableEnhancedValidation = false,
-              },
-              ignoreSingleFileWarning = true,
-            },
-          },
-        },
-      },
-      setup = {
-        terraformls = function(_)
-          return true
-        end,
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        terraform = { "tflint" },
-        tf = { "tflint" },
-      },
-      linters = {
-        tflint = {
-          args = {
-            "--disable-rule=terraform_required_providers",
-            "--format=json",
           },
         },
       },
