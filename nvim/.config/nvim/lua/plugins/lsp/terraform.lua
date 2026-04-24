cc20de238ce87602e5738ee1f8423636a84afa36
Irwin Rex <irwin.a@equipohealth.com>
2026-04-24 16:51:15 +0530

    nvim : updated terraform.lua with less noice

--- nvim/.config/nvim/lua/plugins/lsp/terraform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/terraform.lua
@@ -1,4 +1,3 @@
--- lua/plugins/terraform.lua
 return {
   {
     "neovim/nvim-lspconfig",
@@ -7,39 +6,25 @@ return {
         terraformls = {
           root_dir = function(fname)
             local util = require("lspconfig.util")
-            return util.root_pattern(".terraform", "terragrunt.hcl", ".terraform.lock.hcl", ".git")(fname)
-              or util.root_pattern("main.tf", "variables.tf")(fname)
+            return util.root_pattern(".terraform", ".terraform.lock.hcl", "terragrunt.hcl", "main.tf", ".git")(fname)
               or vim.fs.dirname(fname)
-          end,
-          capabilities = {
-            workspace = {
-              didChangeWatchedFiles = {
-                dynamicRegistration = true,
-              },
-            },
-          },
-          on_attach = function(client, _)
-            client.server_capabilities.semanticTokensProvider = nil
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
           end,
           settings = {
             ["terraform-ls"] = {
               ignoreSingleFileWarning = true,
-              validation = { enableEnhancedValidation = false },
-              indexing = {
-                maxDirectories = 500,
-                ignorePaths = { ".terraform", "**/.terraform", ".git", "**/node_modules" },
-                ignoreDirectoryNames = { ".terraform", "node_modules", ".git" },
-              },
-            },
-            terraform = {
-              experimentalFeatures = {
-                prefillRequiredFields = true,
-                validateOnSave = false,
-              },
             },
           },
+          on_attach = function(client, bufnr)
+            client.server_capabilities.semanticTokensProvider = nil
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function()
+                local fname = vim.api.nvim_buf_get_name(bufnr)
+                vim.fn.system({ "terraform", "fmt", fname })
+                vim.cmd("silent! edit!")
+              end,
+            })
+          end,
         },
       },
     },
